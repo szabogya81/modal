52576768a539ffba41efd0bf7b5242ec66187d75
RaiN <szabogya81@gmail.com>
2020-11-17 21:38:35 +0100

    Modal fade effect fix

--- js/modal.js
+++ js/modal.js
@@ -1,4 +1,57 @@
+const btn = document.querySelector('.showModal');
+const overlayDiv = document.createElement('div');
+overlayDiv.className = 'overlay';
+const modalDiv = document.getElementById('modal');
+window.addEventListener('click', outsideClick);
+
 function closeModal() {
-    let modalDiv = document.getElementById('modal');
-    modalDiv.classList.add("modal--closed");
+    fadeOut(modalDiv);
+       setTimeout(()=>{
+        modalDiv.classList.remove('modal--active');
+        document.body.removeChild(overlayDiv);
+       }, 1300)   
+}
+
+function showModal() {
+    document.body.appendChild(overlayDiv);
+    fadeIn(modalDiv);
+    modal.classList.add('modal--active');
+    modalDiv.focus();
+}
+
+function fadeIn(element) { 
+    let opacity = 0;
+
+    let intervalId = setInterval(function() {
+        if (!element.style.opacity) { 
+            element.style.opacity = 1; 
+        }
+        if(opacity <= 1) {
+            opacity += 0.1;
+            element.style.opacity = opacity;
+        } else {
+            clearInterval(intervalId); 
+        }
+    }, 100); 
+}
+
+function fadeOut(element) { 
+    let opacity = 1;
+
+    let intervalID = setInterval(function () {          
+        if (!element.style.opacity) { 
+            element.style.opacity = 1; 
+        }
+        if (element.style.opacity > 0) { 
+            element.style.opacity -= 0.1; 
+        } else { 
+            clearInterval(intervalID); 
+        }          
+    }, 100);
+} 
+
+function outsideClick(e) {
+    if(e.target === overlayDiv) {
+      closeModal();
+   }
 }
